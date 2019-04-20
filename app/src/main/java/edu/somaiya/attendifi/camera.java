0fffd9b63174b8f4e30c10b30d1ec8c667ec713b
NightWing1998 <dhruvil.s@somaiya.edu>
2019-04-20 22:10:08 +0530

    green tick and camera release

--- app/src/main/java/edu/somaiya/attendifi/camera.java
+++ app/src/main/java/edu/somaiya/attendifi/camera.java
@@ -1,19 +1,14 @@
 package edu.somaiya.attendifi;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
-import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
